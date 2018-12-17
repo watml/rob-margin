bfc564b63a35a2aa2eb613d11fc99dcb90934a6b
Kaiwen <kwwu2015@gmail.com>
2018-12-17 15:55:55 -0600

    Delete .sh file

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python testModel.py MNISTLR ./Model/MNISTLR/MNISTLR_00009.tar MNIST -ckpt 1
-
-#python testModel.py CIFARLR ./Model/CIFARLR.pt CIFAR
-
-#python testModel.py MNISTLR ./Model/MNISTLR.pt MNIST
-
-#python testModel.py MNISTLR ./Backup/Model/MNISTLR.pt MNIST
