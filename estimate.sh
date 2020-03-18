8f51740663b204b0401196091f70fcd8ccdebc53
Kaiwen <kwwu2015@gmail.com>
2020-03-18 17:11:33 -0500

    push script

--- estimate.sh
+++ estimate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python estimateLipschitzBound.py MNISTLR ./Model/MNISTLR/MNISTLR_00010.tar ./Output/MNISTLR/MNISTLR_00010.pkl MNIST -train 0 -n 500 -Nb 1 -Ns 1 -R 5 -p 2 -ckpt 1
+python estimateLipschitzBound.py MNISTLR ./Model/MNISTLR/MNISTLR_01000.tar ./Output/MNISTLR/MNISTLR_00010.pkl MNIST -train 0 -n 500 -Nb 1 -Ns 1 -R 5 -p 2 -ckpt 1
 
